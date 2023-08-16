4667309940783a20ecfd05df309f1aa9297eb60f
anthony <anthonyshao@gmail.com>
2023-08-16 10:09:12 -0700

    first commit

---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,17 @@
+import{ sidebarLinks } from "@/constants"
+
+function LeftSidebar() {
+    return (
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+               {sidebarLinks.map((link) => (
+                <div>
+                    Link
+                </div>
+               ))}
+            </div>
+        </section>
+    )
+}
+
+export default LeftSidebar;

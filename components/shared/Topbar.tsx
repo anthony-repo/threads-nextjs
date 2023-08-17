58540288667f7690768795539b46cdb752d27e9b
anthony <anthonyshao@gmail.com>
2023-08-16 18:35:31 -0700

    fixed minor issues

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -11,32 +11,16 @@ function Topbar() {
                         Threads
                     </p>
             </Link>
-            
-            <div className="flex iteems-center gap-1">
-                <div className="block md:hidden">
-                    <SignedIn>
-                        <SignOutButton>
-                            <div className="flex cursor-pointer">
-                                <Image
-                                    src="/assets/logout.svg"
-                                    alt="logout"
-                                    width={24}
-                                    height={24}
-                                />
-                            </div>
-                        </SignOutButton>
-                    </SignedIn>
-                </div>
                 
-                <OrganizationSwitcher
-                    appearance={{
-                        baseTheme: dark,
-                        elements: {
-                            organizationSwitcherTrigger: "py-2 px-4"
-                        }
-                    }}
-                />
-            </div>
+            <OrganizationSwitcher
+                appearance={{
+                    baseTheme: dark,
+                    elements: {
+                        organizationSwitcherTrigger: "py-2 px-4"
+                    }
+                }}
+            />
+            
         </nav>
     )
 }
